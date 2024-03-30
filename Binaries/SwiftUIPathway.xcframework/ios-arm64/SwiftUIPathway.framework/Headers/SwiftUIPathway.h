0391f3dc70095c0b8e67d992b873708d5265f9e6
Mohammad Mugish <mohammad.mugish@gmail.com>
2024-03-30 07:35:02 +0530

    initial commit

---
+++ Binaries/SwiftUIPathway.xcframework/ios-arm64/SwiftUIPathway.framework/Headers/SwiftUIPathway.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUIPathway.h
+//  SwiftUIPathway
+//
+//  Created by mohammad mugish on 30/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIPathway.
+FOUNDATION_EXPORT double SwiftUIPathwayVersionNumber;
+
+//! Project version string for SwiftUIPathway.
+FOUNDATION_EXPORT const unsigned char SwiftUIPathwayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIPathway/PublicHeader.h>
+
+
